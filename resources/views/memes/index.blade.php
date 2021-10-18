87de31cb0479d1359e5829dd69d45ecca0534500
p4nd4ta <stu1901681077@uni-plovdiv.bg>
2021-10-18 22:49:53 +0300

    [UI Fix] Made images container to be with the same dimmensions on the index page

--- resources/views/memes/index.blade.php
+++ resources/views/memes/index.blade.php
@@ -11,11 +11,11 @@
 
     @forelse($memes as $meme)
     <div class="mt-5 mb-5 d-flex justify-content-center container" id="meme_id_{{$meme->id}}">
-        <div class="row">
-            <div class="col-lg-10 d-flex align-items-stretch">
+        <div class="row w-75 h-auto">
+            <div class="col-lg-10 d-flex justify-content-center">
         <div class="card">
             <div class="card-header h3">{{ $meme->title }}</div>
-            <img class="card-img-top" src="{{ asset('images/' . $meme->image_path) }}" />
+            <img class="card-img-top img-thumbnail" src="{{ asset('images/' . $meme->image_path) }}" />
             <div class="card-footer">
                 <span class="text-success font-weight-bolder" id="up_votes_count{{$meme->id}}">{{ $meme->up_votes_count }}</span> <button onclick="upvote({{$meme->id}})" class="btn btn-success"><i class="fas fa-arrow-up"></i></button>
                 <button onclick="downvote({{$meme->id}})" class="btn btn-danger"><i class="fas fa-arrow-down"></i></button>  <span class="text-danger font-weight-bolder" id="down_votes_count{{$meme->id}}">{{ $meme->down_votes_count }}</span>
